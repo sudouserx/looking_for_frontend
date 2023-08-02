7ca428e61a4319036b98f1f39b532de04ee0add8
sudouserx <sudouserx@gmail.com>
2023-08-02 17:54:59 +0530

    adding splash and Fab color change ( primary --> secondary )

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import HomePage from "./pages/HomePage";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Splash from "./components/Splash"; // Import the Splash component
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 2000);
+  }, []);
+
+  if (loading) {
+    return <Splash />;
+  }
 
   return (
     <div className="app">
@@ -10,6 +23,8 @@ function App() {
         <CssBaseline />
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </BrowserRouter>
     </div>
